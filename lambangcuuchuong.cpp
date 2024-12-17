e4401ecd7c1d992e7e5b9082cdfa02790595fa34
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2024-12-17 08:55:27 +0700

    Update lambangcuuchuong.cpp

--- lambangcuuchuong.cpp
+++ lambangcuuchuong.cpp
@@ -2,11 +2,11 @@
 
 	int main(){
 		int n,i;
-		for(n=1;n<=10;n++){
+		printf("Nhap so Nguyen duong N tu 1-10: ");
+		scanf("%d",&n);
 			printf("Bang cuu chuong cua %d la: \n",n);
 			for (i=1;i<=10;i++){
 				printf(" %d * %d = %d \n", n,i, n*i);	
 		}
-	}
 		return 0;
 	}
